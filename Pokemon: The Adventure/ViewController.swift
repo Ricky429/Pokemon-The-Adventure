6110d88540090e18d1e1b1e29ecf3611212602f3
Ricky429 <ehemingway1106@gmail.com>
2018-03-04 12:51:41 -0600

    Initial Commit

---
+++ Pokemon: The Adventure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon: The Adventure
+//
+//  Created by Erik Hemingway on 3/4/18.
+//  Copyright © 2018 Erik Hemingway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pokemon: The AdventureTests/Pokemon__The_AdventureTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pokemon__The_AdventureTests.swift
+//  Pokemon: The AdventureTests
+//
+//  Created by Erik Hemingway on 3/4/18.
+//  Copyright © 2018 Erik Hemingway. All rights reserved.
+//
+
+import XCTest
+@testable import Pokemon__The_Adventure
+
+class Pokemon__The_AdventureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
